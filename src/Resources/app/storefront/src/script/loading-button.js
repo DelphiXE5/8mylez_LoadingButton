fcdfbee45b3ef5d948f14285d13f5ff423e874ce
= <=>
2022-10-11 01:16:35 +0200

    Added 1 sec loading to the Cart Button with loading indicator

--- src/Resources/app/storefront/src/script/loading-button.js
+++ src/Resources/app/storefront/src/script/loading-button.js
@@ -1,25 +1,33 @@
 import AddToCart from "src/plugin/add-to-cart/add-to-cart.plugin";
 import Iterator from 'src/helper/iterator.helper';
+import DomAccess from "src/helper/dom-access.helper";
 
 export default class QuantityFieldPlugin extends AddToCart {
   init() {
     super.init();
+    this.cartButton = DomAccess.querySelector(this.el, ".btn-buy");
+    this.buttonNormalState = this.cartButton.innerHTML;
+    console.log(this.buttonNormalState);
   }
 
   /**
-     *
+     * @override
+     * 
      * @param {string} requestUrl
      * @param {{}|FormData} formData
      * @private
      */
+
   _openOffCanvasCarts(requestUrl, formData) {
 
-    console.log("Shake it off")
+    this.cartButton.classList.add("btn-loading");
+    this.cartButton.innerHTML = '<div class="loader"></div><span>Wird in den Warenkorb gelegt</span>';
 
     setTimeout(() => {
-      const offCanvasCartInstances = PluginManager.getPluginInstances('OffCanvasCart');
-      Iterator.iterate(offCanvasCartInstances, instance => this._openOffCanvasCart(instance, requestUrl, formData));
-    }, 3000);
+      this.cartButton.classList.remove("btn-loading");
+      this.cartButton.innerHTML = this.buttonNormalState;
+      super._openOffCanvasCarts(requestUrl, formData);
+    }, 1000);
   }
 
 }
