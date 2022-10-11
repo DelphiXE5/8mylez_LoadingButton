db1b316a5a9a513e6cc5d14d2c43fa4205dd26b8
= <=>
2022-10-11 02:41:06 +0200

    Added option to change loadingstate text of Button

--- src/Resources/app/storefront/src/script/loading-button.js
+++ src/Resources/app/storefront/src/script/loading-button.js
@@ -1,13 +1,12 @@
 import AddToCart from "src/plugin/add-to-cart/add-to-cart.plugin";
-import Iterator from 'src/helper/iterator.helper';
 import DomAccess from "src/helper/dom-access.helper";
 
-export default class QuantityFieldPlugin extends AddToCart {
+export default class LoadingButtonPlugin extends AddToCart {
   init() {
     super.init();
     this.cartButton = DomAccess.querySelector(this.el, ".btn-buy");
     this.buttonNormalState = this.cartButton.innerHTML;
-    console.log(this.buttonNormalState);
+    this.loadingStateCaption = this.options.loadingButtonConfig || "Wird in den Warenkorb gelegt";
   }
 
   /**
@@ -21,7 +20,11 @@ export default class QuantityFieldPlugin extends AddToCart {
   _openOffCanvasCarts(requestUrl, formData) {
 
     this.cartButton.classList.add("btn-loading");
-    this.cartButton.innerHTML = '<div class="loader"></div><span>Wird in den Warenkorb gelegt</span>';
+    this.cartButton.innerHTML = `<div class="loader"></div><span></span>`;
+    
+    // Using the textContent here to prevent malicious HTML from the config being rendered 
+    DomAccess.querySelector(this.cartButton, "span").textContent = this.loadingStateCaption;
+
 
     setTimeout(() => {
       this.cartButton.classList.remove("btn-loading");
